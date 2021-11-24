13686a6bf0e24d355b518c5b85e3818d10725e9b
admin <admin@admin.com>
2021-11-24 12:03:13 +0000

    checommit

--- src/com/tek/CustomerDriver.java
+++ src/com/tek/CustomerDriver.java
@@ -5,13 +5,14 @@ public class CustomerDriver {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		CustomerUtil cu = new CustomerUtil();
-
+         Customer c = new Customer();  
 		cu.printCustomers();
+        cu.printCustomers();
 
         String str="insert into customer values(1,'balaji','dev')";
         System.out.println(str);
-       // CustomerDao cd = new CustomerDao();
-       // cd.insertQuery(str);
+       CustomerDao cd = new CustomerDao();
+       cd.insertQuery(str);
 	}
 
 }
